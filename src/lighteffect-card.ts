e450d8c22ae0872f94c3acdf8711672930d8caeb
Justin Crause <juzz@crause.com>
2024-07-20 20:18:30 +0200

    Bumping to v1.1.1 and fixing the console message

--- src/lighteffect-card.ts
+++ src/lighteffect-card.ts
@@ -1,9 +1,5 @@
-import * as pjson from "../package.json";
+import { version } from "../package.json";
 import "./card/lighteffect-card";
 
 /* eslint no-console: 0 */
-console.info(
-  `%c  LIGHTEFFECT-CARD  \n%c Version ${pjson.version} `,
-  "color: orange; font-weight: bold; background: black",
-  "color: white; font-weight: bold; background: dimgray",
-);
+console.info(`%c💡 LightEffect-Card 💡 - ${version} `, "color: #a3d39c; font-weight: 700;");
